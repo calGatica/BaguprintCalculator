a43485c14f98d500f0abda595e6afacb3f70248b
calGatica <85970588+calGatica@users.noreply.github.com>
2023-03-06 09:20:28 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -64,8 +64,8 @@ n = st.number_input('Número de copias', min_value=1, label_visibility='collapse
 for i, j in zip(Formatos[:-1], valoresFormatos):
     if formato == i:
         VALOR = int(j*valor_Ref*n + n*dVal)
-        st.header('Valor: $ %i' %VALOR)
+        st.header('Valor: :green[$ %i]' %VALOR)
 
 if formato == 'Personalizado':
     VALOR = int(customFormat*valor_Ref*n + n*dVal)
-    st.header('Valor: $ %i' %VALOR)
+    st.header('Valor: :green[$ %i]' %VALOR)
